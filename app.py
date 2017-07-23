f84dcae7c9f290a92057138000e1eb6bba1e7a99
caracena <claudio.aracena2@gmail.com>
2017-07-23 12:55:43 +1000

    translate

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
+    # cuando el endpoint este registrado como webhook, debe mandar de vuelta
+    # el valor de 'hub.challenge' que recibe en los argumentos de la llamada
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
@@ -23,31 +23,31 @@ def verify():
 @app.route('/', methods=['POST'])
 def webhook():
 
-    # endpoint for processing incoming messaging events
+    # endpoint para procesar los mensajes que llegan
 
     data = request.get_json()
-    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+    log(data)  # logging, no necesario en produccion
 
     if data["object"] == "page":
 
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
 
-                if messaging_event.get("message"):  # someone sent us a message
+                if messaging_event.get("message"):  # alguien envia un mensaje
 
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    sender_id = messaging_event["sender"]["id"]        # el facebook ID de la persona enviando el mensaje
+                    recipient_id = messaging_event["recipient"]["id"]  # el facebook ID de la pagina que recibe (tu pagina)
+                    message_text = messaging_event["message"]["text"]  # el texto del mensaje
 
-                    send_message(sender_id, "roger that!")
+                    send_message(sender_id, "hola, esta es la respuesta por defecto")
 
-                if messaging_event.get("delivery"):  # delivery confirmation
+                if messaging_event.get("delivery"):  # confirmacion de delivery
                     pass
 
-                if messaging_event.get("optin"):  # optin confirmation
+                if messaging_event.get("optin"):  # confirmacion de optin
                     pass
 
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                if messaging_event.get("postback"):  # evento cuando usuario hace click en botones
                     pass
 
     return "ok", 200
@@ -77,7 +77,7 @@ def send_message(recipient_id, message_text):
         log(r.text)
 
 
-def log(message):  # simple wrapper for logging to stdout on heroku
+def log(message):  # funcion de logging para heroku
     print str(message)
     sys.stdout.flush()
 
